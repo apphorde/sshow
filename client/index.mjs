e8d9c5f7603910405c0c5a5f99b5a056ccaf8450
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2025-09-22 15:40:31 +0530

    feat: add multi-terminal UI

--- client/index.mjs
+++ client/index.mjs
@@ -1,3 +1,6 @@
+import { ref, defineProp, onInit, onDestroy } from "@li3/web";
+import { templateRef } from "@li3/plugins";
+
 const defaults = {
   maxBuffer: 5,
   debounceTime: 50,
@@ -8,17 +11,6 @@ const config = {
   ...(window.sshow || {}),
 };
 
-function getTerminal() {
-  const terminal = new Terminal({ convertEol: true });
-  terminal.open(document.getElementById("terminal"));
-
-  const fitAddon = new FitAddon.FitAddon();
-  terminal.loadAddon(fitAddon);
-  window.addEventListener("resize", () => fitAddon.fit());
-
-  return { terminal, fitAddon };
-}
-
 function debounce(fn, time = config.debouceTime) {
   let timer;
 
@@ -28,108 +20,145 @@ function debounce(fn, time = config.debouceTime) {
   };
 }
 
-function sendInput() {
-  const data = clientBuffer.join("");
-  onSend({ type: "input", data });
-  clientBuffer.length = 0;
-}
+const addons = new Set();
+window.addEventListener("resize", () => {
+  for (const next of addons) {
+    next.fit();
+  }
+});
 
-let currentSocket;
-let reconnect = true;
-const sendDelayed = debounce(sendInput);
-const clientBuffer = [];
-const { terminal, fitAddon } = getTerminal();
+export default function () {
+  const terminalRef = templateRef("terminal");
+  const reconnect = ref(true);
+  const online = ref(false);
+  const name = defineProp("name", {
+    default() {
+      return "term-" + ~~(Math.random() * 100);
+    },
+  });
 
-function onClientWrite(c) {
-  clientBuffer.push(c);
-
-  if (c === "\r" || clientBuffer.length > config.maxBuffer) {
-    return sendInput();
+  function sendInput() {
+    const data = clientBuffer.join("");
+    onSend({ type: "input", data });
+    clientBuffer.length = 0;
   }
 
-  sendDelayed();
-}
+  let currentSocket;
+  let terminal;
+  let fitAddon;
+  const sendDelayed = debounce(sendInput);
+  const clientBuffer = [];
 
-function onSend(data) {
-  if (currentSocket && currentSocket.OPEN === currentSocket.readyState) {
-    currentSocket.send(JSON.stringify(data));
-  }
-}
+  function createTerminal() {
+    terminal = new Terminal({ convertEol: true });
+    terminal.open(terminalRef.value);
 
-terminal.onData(onClientWrite);
-terminal.onResize(({ cols, rows }) =>
-  onSend({ type: "resize", data: { cols, rows } })
-);
-
-function onStatusChange(online) {
-  const c = document.getElementById("status").classList;
-  c.toggle("bg-green-400", online);
-  c.toggle("bg-red-400", !online);
-
-  if (!online && reconnect) {
-    setTimeout(connect, 5000);
+    fitAddon = new FitAddon.FitAddon();
+    terminal.loadAddon(fitAddon);
+    terminal.onData(onClientWrite);
+    terminal.onResize(({ cols, rows }) =>
+      onSend({ type: "resize", data: { cols, rows } })
+    );
   }
 
-  if (online) {
-    fitAddon.fit();
-  }
-}
+  function onClientWrite(c) {
+    clientBuffer.push(c);
 
-function onClose() {
-  reconnect = false;
+    if (c === "\r" || clientBuffer.length > config.maxBuffer) {
+      return sendInput();
+    }
 
-  if (currentSocket) {
-    onSend({ type: "close" });
-    currentSocket.close();
+    sendDelayed();
   }
 
-  currentSocket = null;
-  terminal.write("\nConnection closed.");
-}
+  function onSend(data) {
+    if (currentSocket && currentSocket.OPEN === currentSocket.readyState) {
+      currentSocket.send(JSON.stringify(data));
+    }
+  }
 
-function onMessage(message) {
-  const event = JSON.parse(message);
+  function onStatusChange(newStatus) {
+    online.value = newStatus;
 
-  switch (event.type) {
-    case "close":
-      onClose();
-      break;
+    if (!newStatus && reconnect.value) {
+      setTimeout(connect, 5000);
+    }
 
-    case "stdout":
-      const chunk = event.data;
+    if (newStatus) {
+      fitAddon.fit();
+    }
+  }
 
-      if (typeof chunk === "string") {
-        terminal.write(chunk);
+  function onClose() {
+    reconnect.value = false;
+
+    if (currentSocket) {
+      onSend({ type: "close" });
+      currentSocket.close();
+    }
+
+    currentSocket = null;
+    terminal.write("\nConnection closed.");
+  }
+
+  function onMessage(message) {
+    const event = JSON.parse(message);
+
+    switch (event.type) {
+      case "close":
+        onClose();
         break;
-      }
 
-      if (chunk.type === "Buffer") {
-        const buffer = new ArrayBuffer(chunk.data.length);
-        const uint8 = new Uint8Array(buffer);
-        uint8.set(chunk.data, 0);
-        terminal.write(uint8);
-      }
-      break;
+      case "stdout":
+        const chunk = event.data;
+
+        if (typeof chunk === "string") {
+          terminal.write(chunk);
+          break;
+        }
+
+        if (chunk.type === "Buffer") {
+          const buffer = new ArrayBuffer(chunk.data.length);
+          const uint8 = new Uint8Array(buffer);
+          uint8.set(chunk.data, 0);
+          terminal.write(uint8);
+        }
+        break;
+    }
   }
+
+  async function connect() {
+    const socket = new WebSocket(
+      location.protocol.replace("http", "ws") +
+        "//" +
+        location.host +
+        "/socket?name=" +
+        name.value
+    );
+
+    socket.addEventListener("message", (e) => onMessage(e.data));
+    socket.addEventListener("close", () => onStatusChange(false));
+    socket.addEventListener("open", () => onStatusChange(true));
+
+    currentSocket = socket;
+    setTimeout(() => fitAddon.fit(), 1000);
+  }
+
+  onInit(() => {
+    createTerminal();
+    connect();
+    addons.add(fitAddon);
+  });
+
+  onDestroy(() => {
+    addons.delete(fitAddon);
+  });
+
+  function onReconnect() {
+    reconnect = true;
+    terminal.clear();
+    connect();
+  }
+
+  return { onReconnect, online, name };
 }
-
-async function connect() {
-  const url = new URL(location.href);
-  const socket = new WebSocket(
-    location.protocol.replace("http", "ws") + "//" + location.host + "/socket?name=" + (url.searchParams.get('name') || '')
-  );
-
-  socket.addEventListener("message", (e) => onMessage(e.data));
-  socket.addEventListener("close", () => onStatusChange(false));
-  socket.addEventListener("open", () => onStatusChange(true));
-
-  currentSocket = socket;
-  setTimeout(() => fitAddon.fit(), 1000);
-}
-
-connect();
-document.getElementById("status").onclick = () => {
-  reconnect = true;
-  terminal.clear();
-  connect();
-};
